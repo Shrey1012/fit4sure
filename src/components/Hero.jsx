146a040440d23ca4a940aed3fadfe8e30d70b013
Parth Chandravadiya <202051053@iiitvadodara.ac.in>
2023-04-21 03:03:42 +0530

    Hero component styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import "./Hero.css";
 import landingRightImg from "../assets/landing right img.svg";
+import outerstar from '../assets/outerstar.svg';
 const Home = () => {
   return (
+    
     <div className="hero-container">
-      <div className="hero-text1">Only workout!? No, It’s all about Fitness.. </div>
-      <div className="hero-text2">
+      <div className="hero_left">
+        <div className="hero-text1">Only workout!? No, It's all about Fitness.. </div>
+        <div className="hero-text2">
         We make you do the exercise you like, provide constant encouragement,
         amend your mindset to live life like a fitness freak. The exercise
         tracking tool and 8 other tools act as the cherry on the cake.
-      </div>
-      <hr className="hero-line"/>
+        </div>
+      <div className="hero-line"/>
       <button className="hero-button">Get Started</button>
+      </div>
+      
       <img className="hero-image" src={landingRightImg} alt="img" />
+      <div className="highlight1"></div>
+      <img className="star-img" src={outerstar} alt="starr" />
     </div>
   );
 };
