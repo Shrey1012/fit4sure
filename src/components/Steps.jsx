7b9bf495a62098c425c6947259330763e8001fee
Parth Chandravadiya <202051053@iiitvadodara.ac.in>
2023-04-22 00:22:24 +0530

    Updated some files

--- src/components/Steps.jsx
+++ src/components/Steps.jsx
@@ -5,8 +5,14 @@ const Steps = () => {
   return (
     <div className='steps-main'>
       <div className='top-text1'>Juat 4 easy steps</div>
-      <div className='top-text2'>Our professionals understand that every person has a different metabolism. We’ll make your weight loss journey a walk in the park with advanced, fun, and practical one-to-one coaching tips, tracking tools, and support groups.</div>
-          
+      <div className='top-text2'>Our professionals understand that every person has a different metabolism. We'll make your weight loss journey a walk in the park with advanced, fun, and practical one-to-one coaching tips, tracking tools, and support groups.</div>
+      <div className='steps-box'>
+        <div className='step1'>
+          <div className='num-txt'>1</div>
+          <div className='steps-line'></div>
+          <div className='steps-txt'>Provide basic information about your body</div>
+        </div>
+      </div>
     </div>
   )
 }
