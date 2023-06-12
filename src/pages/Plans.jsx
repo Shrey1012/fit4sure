804b3ec175b51a01834cc258e6a0e9ff8a6be5ce
Shrey1012 <shrey9157@gmail.com>
2023-06-12 18:50:24 +0530

    Updated plans

--- src/pages/Plans.jsx
+++ src/pages/Plans.jsx
@@ -1,43 +1,54 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './Plans.css'
 import arrownxt from '../assets/arrownxt.svg';
+import axios from 'axios';
 
 const Plans = () => {
+  const [plans, setPlans] = useState([]);
+  const [selectedPlan, setSelectedPlan] = useState(null);
+
+  useEffect(() => {
+    const fetchPlans = async () => {
+      const res = await axios.get('http://localhost:3001/admin/plan/all');
+      setPlans(res.data.plan);
+      setSelectedPlan(res.data.plan.find(plan => plan.duration === '1 Year'));
+    }
+    fetchPlans();
+  }, []);
+
+  const handlePlanClick = (plan) => {
+    setSelectedPlan(plan);
+  };
+
+  const splitDescription = (description) => {
+    const sentences = description.split('. ');
+    return sentences.map((sentence, index) => <li key={index}>{sentence}</li>);
+  };
+
   return (
     <div className='Plans-main'>
       <div className='plans-txt1'>Select a plan</div>
       <div className='plans-txt2'>We make you fit in a sustainable manner by using the science of fitness and providing constant support. Our certified and top-rated professional experts provide long-lasting support, making personalized schedules at times convenient for you.</div>
       <div className='plans-mid'>
-        <div>
-          <h2>1 Year</h2>
-          <p>INR 1299 <span>/month</span></p>
+    {plans.map((plan) => (
+        <div key={plan._id} onClick={() => handlePlanClick(plan)}>
+          <h2>{plan.duration}</h2>
+          <p>INR {plan.price} <span>/month</span></p>
         </div>
-        <div>
-          <h2>6 Months</h2>
-          <p>INR 1749 <span>/month</span></p>
-        </div>
-        <div>
-          <h2>3 Months</h2>
-          <p>INR 1999 <span>/month</span></p>
-        </div>
-        <div>
-          <h2>Monthly</h2>
-          <p>INR 2449 <span>/month</span></p>
-        </div>
+     ))} 
       </div>
       <div className='plans-bottom'>
-        <h2>1 Year plan</h2>
-        <ul>
-          <li>We make you fit in a sustainable manner by using the science of</li>
-          <li>We make you fit in a sustainable manner by using the science of</li>
-          <li>We make you fit science of in a sustainable manner by using the </li>
-          <li>We make you fit in a sustainable manner by using the science of</li>
-          <li>We make you fit in a sustainable the science of manner by using the science of</li>
-          <li>We make you fit in a sustainable manner by using </li>
-        </ul>
+        {selectedPlan ? (
+          <>
+        <h2>{selectedPlan.title}</h2>
+        <ul>{splitDescription(selectedPlan.description)}</ul>
         <button>Go with this plans
           <img src={arrownxt} alt="" />
         </button>
+        </>
+        ) : (
+          <h2>Select a plan to continue</h2>
+        )}
       </div>
     </div>
   )
