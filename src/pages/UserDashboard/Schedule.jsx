97b5bc9bb67e26194ec5625a030b412ba695755a
Shrey1012 <shrey9157@gmail.com>
2023-08-29 04:29:19 +0530

    Updated Schedule

--- src/pages/UserDashboard/Schedule.jsx
+++ src/pages/UserDashboard/Schedule.jsx
@@ -1,81 +1,82 @@
-import React from 'react'
-import './Schedule.css'
+import React, { useState, useEffect } from "react";
+import "./Schedule.css";
+import axios from "axios";
 
 const Schedule = () => {
+  const [scheduleData, setScheduleData] = useState([]);
+
+  const API = axios.create({ baseURL: "http://localhost:3001" });
+
+  API.interceptors.request.use((req) => {
+    if (localStorage.getItem("profile")) {
+      req.headers.authorization = `Bearer ${
+        JSON.parse(localStorage.getItem("profile")).token
+      }`;
+    }
+
+    return req;
+  });
+
+  useEffect(() => {
+    const fetchScheduleData = async () => {
+      try {
+        const response = await API.get("/app/schedule");
+        console.log(response.data.schedule);
+        setScheduleData(response.data.schedule);
+      } catch (error) {
+        console.error("Error fetching schedule data:", error);
+      }
+    };
+    fetchScheduleData();
+  }, []);
+
   return (
-    <div className='Schedule-main'>
-      <h2 className='Schedule-title'>Your upcoming schedules</h2>
+    <div className="Schedule-main">
+      <h2 className="Schedule-title">Your upcoming schedules</h2>
       <div class="user_table px-xl-5 px-lg-3 mt-4">
-                        <div class="table-responsive">
-                            <table class="table">
-                                <thead>
-                                    <tr>
-                                        <th>Coach name</th>
-                                        <th>Date</th>
-                                        <th>Meeting link</th>
-                                        <th>Meeting ID</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
+        <div class="table-responsive">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>Coach name</th>
+                <th>Date</th>
+                <th>Meeting link</th>
+                <th>Meeting ID</th>
+                <th>Password</th>
+              </tr>
+            </thead>
 
-                                <tbody>
-                                    <tr>
-                                        <td data-label="Coach name">Ayush singh Verma</td>
-                                        <td data-label="Date">27 July, 2:30 pm onwards</td>
-                                        <td data-label="Meeting link"><a href="">https://meet.google.com/oqm-gnsi-tgo</a></td>
-                                        <td data-label="Meeting ID">oqm-gnsi-tgo </td>
-                                        <td data-label="">
-                                            <div>
-                                                <span class="me-2"><svg width="56" height="6" viewBox="0 0 56 6" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="3" cy="3" r="3" fill="#B9B9B9"/><circle cx="13" cy="3" r="3" fill="#B9B9B9"/><circle cx="23" cy="3" r="3" fill="#B9B9B9"/><circle cx="33" cy="3" r="3" fill="#B9B9B9"/><circle cx="43" cy="3" r="3" fill="#B9B9B9"/><circle cx="53" cy="3" r="3" fill="#B9B9B9"/></svg></span>
-                                                <a href=""><svg width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.9833 8.00001C11.9833 9.65001 10.6499 10.9833 8.99993 10.9833C7.34993 10.9833 6.0166 9.65001 6.0166 8.00001C6.0166 6.35001 7.34993 5.01668 8.99993 5.01668C10.6499 5.01668 11.9833 6.35001 11.9833 8.00001Z" stroke="#292D32" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/><path d="M9.00011 14.8917C11.9418 14.8917 14.6834 13.1583 16.5918 10.1583C17.3418 8.98333 17.3418 7.00833 16.5918 5.83333C14.6834 2.83333 11.9418 1.1 9.00011 1.1C6.05845 1.1 3.31678 2.83333 1.40845 5.83333C0.658447 7.00833 0.658447 8.98333 1.40845 10.1583C3.31678 13.1583 6.05845 14.8917 9.00011 14.8917Z" stroke="#292D32" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/></svg></a>
-                                            </div>
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <td data-label="Coach name">Ayush singh Verma</td>
-                                        <td data-label="Date">27 July, 2:30 pm onwards</td>
-                                        <td data-label="Meeting link"><a href="">https://meet.google.com/oqm-gnsi-tgo</a></td>
-                                        <td data-label="Meeting ID">oqm-gnsi-tgo </td>
-                                        <td data-label="">
-                                            <div>
-                                                <span class="me-2"><svg width="56" height="6" viewBox="0 0 56 6" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="3" cy="3" r="3" fill="#B9B9B9"/><circle cx="13" cy="3" r="3" fill="#B9B9B9"/><circle cx="23" cy="3" r="3" fill="#B9B9B9"/><circle cx="33" cy="3" r="3" fill="#B9B9B9"/><circle cx="43" cy="3" r="3" fill="#B9B9B9"/><circle cx="53" cy="3" r="3" fill="#B9B9B9"/></svg></span>
-                                                <a href=""><svg width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.9833 8.00001C11.9833 9.65001 10.6499 10.9833 8.99993 10.9833C7.34993 10.9833 6.0166 9.65001 6.0166 8.00001C6.0166 6.35001 7.34993 5.01668 8.99993 5.01668C10.6499 5.01668 11.9833 6.35001 11.9833 8.00001Z" stroke="#292D32" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/><path d="M9.00011 14.8917C11.9418 14.8917 14.6834 13.1583 16.5918 10.1583C17.3418 8.98333 17.3418 7.00833 16.5918 5.83333C14.6834 2.83333 11.9418 1.1 9.00011 1.1C6.05845 1.1 3.31678 2.83333 1.40845 5.83333C0.658447 7.00833 0.658447 8.98333 1.40845 10.1583C3.31678 13.1583 6.05845 14.8917 9.00011 14.8917Z" stroke="#292D32" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/></svg></a>
-                                            </div>
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <td data-label="Coach name">Ayush singh Verma</td>
-                                        <td data-label="Date">27 July, 2:30 pm onwards</td>
-                                        <td data-label="Meeting link"><a href="">https://meet.google.com/oqm-gnsi-tgo</a></td>
-                                        <td data-label="Meeting ID">oqm-gnsi-tgo </td>
-                                        <td data-label="">
-                                            <div>
-                                                <span class="me-2"><svg width="56" height="6" viewBox="0 0 56 6" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="3" cy="3" r="3" fill="#B9B9B9"/><circle cx="13" cy="3" r="3" fill="#B9B9B9"/><circle cx="23" cy="3" r="3" fill="#B9B9B9"/><circle cx="33" cy="3" r="3" fill="#B9B9B9"/><circle cx="43" cy="3" r="3" fill="#B9B9B9"/><circle cx="53" cy="3" r="3" fill="#B9B9B9"/></svg></span>
-                                                <a href=""><svg width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.9833 8.00001C11.9833 9.65001 10.6499 10.9833 8.99993 10.9833C7.34993 10.9833 6.0166 9.65001 6.0166 8.00001C6.0166 6.35001 7.34993 5.01668 8.99993 5.01668C10.6499 5.01668 11.9833 6.35001 11.9833 8.00001Z" stroke="#292D32" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/><path d="M9.00011 14.8917C11.9418 14.8917 14.6834 13.1583 16.5918 10.1583C17.3418 8.98333 17.3418 7.00833 16.5918 5.83333C14.6834 2.83333 11.9418 1.1 9.00011 1.1C6.05845 1.1 3.31678 2.83333 1.40845 5.83333C0.658447 7.00833 0.658447 8.98333 1.40845 10.1583C3.31678 13.1583 6.05845 14.8917 9.00011 14.8917Z" stroke="#292D32" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/></svg></a>
-                                            </div>
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <td data-label="Coach name">Ayush singh Verma</td>
-                                        <td data-label="Date">27 July, 2:30 pm onwards</td>
-                                        <td data-label="Meeting link"><a href="">https://meet.google.com/oqm-gnsi-tgo</a></td>
-                                        <td data-label="Meeting ID">oqm-gnsi-tgo </td>
-                                        <td data-label="">
-                                            <div>
-                                                <span class="me-2"><svg width="56" height="6" viewBox="0 0 56 6" fill="none" xmlns="http://www.w3.org/2000/svg"><circle cx="3" cy="3" r="3" fill="#B9B9B9"/><circle cx="13" cy="3" r="3" fill="#B9B9B9"/><circle cx="23" cy="3" r="3" fill="#B9B9B9"/><circle cx="33" cy="3" r="3" fill="#B9B9B9"/><circle cx="43" cy="3" r="3" fill="#B9B9B9"/><circle cx="53" cy="3" r="3" fill="#B9B9B9"/></svg></span>
-                                                <a href=""><svg width="18" height="16" viewBox="0 0 18 16" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.9833 8.00001C11.9833 9.65001 10.6499 10.9833 8.99993 10.9833C7.34993 10.9833 6.0166 9.65001 6.0166 8.00001C6.0166 6.35001 7.34993 5.01668 8.99993 5.01668C10.6499 5.01668 11.9833 6.35001 11.9833 8.00001Z" stroke="#292D32" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/><path d="M9.00011 14.8917C11.9418 14.8917 14.6834 13.1583 16.5918 10.1583C17.3418 8.98333 17.3418 7.00833 16.5918 5.83333C14.6834 2.83333 11.9418 1.1 9.00011 1.1C6.05845 1.1 3.31678 2.83333 1.40845 5.83333C0.658447 7.00833 0.658447 8.98333 1.40845 10.1583C3.31678 13.1583 6.05845 14.8917 9.00011 14.8917Z" stroke="#292D32" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/></svg></a>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        </div>              
+            <tbody>
+              {scheduleData?.map((schedule) => (
+                <tr>
+                  <td data-label="Coach name">{schedule?.trainer_id?.name}</td>
+                  <td data-label="Date">
+                    {schedule.date}, {schedule.time}{" "}
+                    {schedule.time >= "12:00" ? "pm" : "am"} onwards
+                  </td>
+                  <td data-label="Meeting link">
+                    <a
+                      href={schedule.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {schedule.link}
+                    </a>
+                  </td>
+                  <td data-label="Meeting ID">{schedule.meeting_id}</td>
+                  <td data-label="Meeting Password">
+                    {schedule.meeting_password
+                      ? schedule.meeting_password
+                      : "No password"}
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Schedule
+export default Schedule;
